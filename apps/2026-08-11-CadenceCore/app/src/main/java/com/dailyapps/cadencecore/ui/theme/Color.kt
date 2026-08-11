6f966ca5d2677481646a55ed7af53f260f0f030e
Faizan-dev <ahmedfaizan931star@gmail.com>
2026-08-11 10:46:29 +0500

    CadenceCore: Add manifest, resources, theme and MainActivity

---
+++ apps/2026-08-11-CadenceCore/app/src/main/java/com/dailyapps/cadencecore/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.dailyapps.cadencecore.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Teal80 = Color(0xFF5EEAD4)
+val TealGrey80 = Color(0xFF99F6E4)
+val Coral80 = Color(0xFFFDA4AF)
+
+val Teal40 = Color(0xFF0F766E)
+val TealGrey40 = Color(0xFF134E4A)
+val Coral40 = Color(0xFFE11D48)
+
+val SurfaceLight = Color(0xFFF0FDFA)
+val SurfaceDark = Color(0xFF042F2E)
+val OnSurfaceLight = Color(0xFF134E4A)
+val OnSurfaceDark = Color(0xFFCCFBF1)
