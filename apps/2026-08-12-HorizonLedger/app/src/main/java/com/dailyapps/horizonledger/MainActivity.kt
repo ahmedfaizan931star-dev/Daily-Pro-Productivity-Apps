c870fcaa1b1d7b5ab1b13d6094ad1653395ae304
Faizan-dev <ahmedfaizan931star@gmail.com>
2026-08-12 10:50:15 +0500

    Fix MainActivity typo

--- apps/2026-08-12-HorizonLedger/app/src/main/java/com/dailyapps/horizonledger/MainActivity.kt
+++ apps/2026-08-12-HorizonLedger/app/src/main/java/com/dailyapps/horizonledger/MainActivity.kt
@@ -14,7 +14,7 @@ import com.dailyapps.horizonledger.viewmodel.LedgerViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceedInstanceState)
+        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             HorizonLedgerTheme {
