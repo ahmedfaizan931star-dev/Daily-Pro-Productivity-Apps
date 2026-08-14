c33eee0ca7e35e2b25c85b229bdc1a389a5ba04a
Faizan-dev <ahmedfaizan931star@gmail.com>
2026-08-14 10:51:28 +0500

    ApexFlow: add theme, viewmodel, components, navigation, screens, gradlew

---
+++ apps/2026-08-14-ApexFlow/app/src/main/java/com/dailyapps/apexflow/ui/theme/Color.kt
@@ -0,0 +1,23 @@
+package com.dailyapps.apexflow.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val ApexPrimary = Color(0xFF0EA5E9)
+val ApexOnPrimary = Color(0xFFFFFFFF)
+val ApexSecondary = Color(0xFF6366F1)
+val ApexTertiary = Color(0xFF14B8A6)
+val ApexBackground = Color(0xFFF0F9FF)
+val ApexSurface = Color(0xFFFFFFFF)
+val ApexOnBackground = Color(0xFF0F172A)
+val ApexOnSurface = Color(0xFF1E293B)
+
+val ApexPrimaryNight = Color(0xFF38BDF8)
+val ApexBackgroundNight = Color(0xFF0F172A)
+val ApexSurfaceNight = Color(0xFF1E293B)
+val ApexOnBackgroundNight = Color(0xFFF1F5F9)
+val ApexOnSurfaceNight = Color(0xFFE2E8F0)
+
+val ApexHigh = Color(0xFFEF4444)
+val ApexMedium = Color(0xFFF59E0B)
+val ApexLow = Color(0xFF22C55E)
+val ApexAccent = Color(0xFF8B5CF6)
