2b101afce72f5e12c0d09454ec0cd51cba5fece8
Faizan-dev <ahmedfaizan931star@gmail.com>
2026-08-14 19:21:14 +0500

    Proper Modifier import

--- apps/2026-08-14-ApexFlow/app/src/main/java/com/dailyapps/apexflow/ui/screens/FocusScreen.kt
+++ apps/2026-08-14-ApexFlow/app/src/main/java/com/dailyapps/apexflow/ui/screens/FocusScreen.kt
@@ -23,7 +23,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.Modifier.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
