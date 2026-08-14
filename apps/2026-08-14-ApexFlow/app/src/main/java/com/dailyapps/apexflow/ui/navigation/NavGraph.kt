62075b55c0add943b2cd753d59041960096d9b15
Faizan-dev <ahmedfaizan931star@gmail.com>
2026-08-14 19:22:29 +0500

    Fix Modifier import in NavGraph

--- apps/2026-08-14-ApexFlow/app/src/main/java/com/dailyapps/apexflow/ui/navigation/NavGraph.kt
+++ apps/2026-08-14-ApexFlow/app/src/main/java/com/dailyapps/apexflow/ui/navigation/NavGraph.kt
@@ -13,7 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.Modifier.modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavDestination.Companion.hierarchy
